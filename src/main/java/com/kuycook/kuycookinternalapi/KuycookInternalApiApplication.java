a81a4b36b78fba2fe7e64fb0372404a5c6cc5009
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-01-22 10:05:09 +0700

    feat: initial project 🎉

---
+++ src/main/java/com/kuycook/kuycookinternalapi/KuycookInternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.kuycook.kuycookinternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuycookInternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuycookInternalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuycook/kuycookinternalapi/KuycookInternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuycook.kuycookinternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuycookInternalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
