2376f06569935d1005fb046bee02f80028db97e7
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-06-26 17:06:29 +0700

    refactor: refactor Employee using UserApi

--- src/main/java/com/kkp/pelatihanwebservice/internal/controllers/EmployeeController.java
+++ src/main/java/com/kkp/pelatihanwebservice/internal/controllers/EmployeeController.java
@@ -2,10 +2,10 @@ package com.kkp.pelatihanwebservice.internal.controllers;
 
 import com.kkp.pelatihanwebservice.internal.dto.employee.request.EmployeeRequest;
 import com.kkp.pelatihanwebservice.internal.dto.employee.response.EmployeeResponse;
-import com.kkp.pelatihanwebservice.internal.models.Employee;
-import com.kkp.pelatihanwebservice.internal.repositories.EmployeeRepository;
-import com.kkp.pelatihanwebservice.internal.services.employee.EmployeeService;
-import com.kkp.pelatihanwebservice.internal.services.employee.EmployeeServiceImpl;
+import com.kkp.pelatihanwebservice.internal.models.UserApi;
+import com.kkp.pelatihanwebservice.internal.repositories.UserApiRepository;
+import com.kkp.pelatihanwebservice.internal.services.userApi.UserApiService;
+import com.kkp.pelatihanwebservice.internal.services.userApi.UserApiServiceImpl;
 import com.kkp.pelatihanwebservice.internal.utils.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -26,26 +26,26 @@ import java.io.IOException;
 @RequestMapping("/pelatihan-webservice/internal-api/v1/employees")
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+    private UserApiService userApiService;
 
     @Autowired
-    private EmployeeServiceImpl employeeServiceImpl;
+    private UserApiServiceImpl employeeServiceImpl;
 
     @Autowired
-    EmployeeRepository employeeRepository;
+    UserApiRepository userApiRepository;
 
     @Autowired
     PasswordEncoder encoder;
 
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+    public EmployeeController(UserApiService userApiService) {
+        this.userApiService = userApiService;
     }
 
     @GetMapping("")
-    public Iterable<Employee> employeeList(@RequestParam(required = false, value = "search", defaultValue = "") String searhData,
-                                           @RequestParam(required = false, value = "size", defaultValue = "5") int size,
-                                           @RequestParam(required = false, value = "page", defaultValue = "0") int page,
-                                           @RequestParam(required = false, value = "sort", defaultValue = "desc") String sort)
+    public Iterable<UserApi> employeeList(@RequestParam(required = false, value = "search", defaultValue = "") String searhData,
+                                          @RequestParam(required = false, value = "size", defaultValue = "5") int size,
+                                          @RequestParam(required = false, value = "page", defaultValue = "0") int page,
+                                          @RequestParam(required = false, value = "sort", defaultValue = "desc") String sort)
             throws IOException {
         try {
             Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
@@ -67,11 +67,11 @@ public class EmployeeController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<EmployeeResponse<Employee>> employeeCreate(@Valid @RequestBody EmployeeRequest employeeRequest,
-                                                                     Errors errors) {
-        EmployeeResponse<Employee> responseData = new EmployeeResponse<>();
+    public ResponseEntity<EmployeeResponse<UserApi>> employeeCreate(@Valid @RequestBody EmployeeRequest employeeRequest,
+                                                                    Errors errors) {
+        EmployeeResponse<UserApi> responseData = new EmployeeResponse<>();
 
-        if (employeeRepository.existsEmployeeByEmail(employeeRequest.getEmail())) {
+        if (userApiRepository.existsEmployeeByEmail(employeeRequest.getEmail())) {
             String[] errorMessages = {
                     "Email sudah digunakan"
             };
@@ -94,19 +94,19 @@ public class EmployeeController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
 
-        Employee employee = new Employee(employeeRequest.getName(), employeeRequest.getEmail(), encoder.encode(employeeRequest.getPassword()),
-                employeeRequest.getEmailVerifiedAt(), employeeRequest.getCreatedAt(), employeeRequest.getUpdatedAt());
+        UserApi userApi = new UserApi(employeeRequest.getName(), employeeRequest.getEmail(), encoder.encode(employeeRequest.getPassword()),
+                employeeRequest.getEmailVerifiedAt(), employeeRequest.isAdmin(), employeeRequest.getCreatedAt(), employeeRequest.getUpdatedAt());
         responseData.setStatus(true);
-        responseData.setData(employeeServiceImpl.createEmployee(employee));
+        responseData.setData(employeeServiceImpl.createEmployee(userApi));
         responseData.setCode(200);
 
         return ResponseEntity.ok(responseData);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EmployeeResponse<Employee>> employeeUpdate(@Valid @RequestBody EmployeeRequest employeeRequest,
-                                                                     Errors errors, @PathVariable("id") Long id) {
-        EmployeeResponse<Employee> responseData = new EmployeeResponse<>();
+    public ResponseEntity<EmployeeResponse<UserApi>> employeeUpdate(@Valid @RequestBody EmployeeRequest employeeRequest,
+                                                                    Errors errors, @PathVariable("id") Long id) {
+        EmployeeResponse<UserApi> responseData = new EmployeeResponse<>();
 
         if (employeeRequest.getEmail().equals(employeeServiceImpl.findEmployeeById(id).getEmail())) {
             if (errors.hasErrors()) {
@@ -119,16 +119,17 @@ public class EmployeeController {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
             }
 
-            Employee employee = new Employee(employeeRequest.getName(), employeeRequest.getEmail(), encoder.encode(employeeRequest.getPassword()),
-                    employeeRequest.getEmailVerifiedAt(), employeeRequest.getCreatedAt(), employeeRequest.getUpdatedAt());
+            UserApi userApi = new UserApi(employeeRequest.getName(), employeeRequest.getEmail(), encoder.encode(employeeRequest.getPassword()),
+                    employeeRequest.getEmailVerifiedAt(), employeeRequest.isAdmin(), employeeRequest.getCreatedAt(),
+                    employeeRequest.getUpdatedAt());
             responseData.setStatus(true);
             responseData.setCode(200);
-            responseData.setData(employeeServiceImpl.updateEmployee(id, employee));
+            responseData.setData(employeeServiceImpl.updateEmployee(id, userApi));
             return ResponseEntity.ok(responseData);
         }
 
 
-        if (employeeRepository.existsEmployeeByEmail(employeeRequest.getEmail())) {
+        if (userApiRepository.existsEmployeeByEmail(employeeRequest.getEmail())) {
             String[] errorMessages = {
                     "Email sudah digunakan"
             };
@@ -151,16 +152,16 @@ public class EmployeeController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
 
-        Employee employee = new Employee(employeeRequest.getName(), employeeRequest.getEmail(), encoder.encode(employeeRequest.getPassword()),
-                employeeRequest.getEmailVerifiedAt(), employeeRequest.getCreatedAt(), employeeRequest.getUpdatedAt());
+        UserApi userApi = new UserApi(employeeRequest.getName(), employeeRequest.getEmail(), encoder.encode(employeeRequest.getPassword()),
+                employeeRequest.getEmailVerifiedAt(), employeeRequest.isAdmin(), employeeRequest.getCreatedAt(), employeeRequest.getUpdatedAt());
         responseData.setStatus(true);
         responseData.setCode(200);
-        responseData.setData(employeeServiceImpl.updateEmployee(id, employee));
+        responseData.setData(employeeServiceImpl.updateEmployee(id, userApi));
         return ResponseEntity.ok(responseData);
     }
 
     @DeleteMapping("/{id}")
-    public Employee employeeDelete(@PathVariable("id") Long id) {
+    public UserApi employeeDelete(@PathVariable("id") Long id) {
         return employeeServiceImpl.deleteEmployee(id);
     }
 }
